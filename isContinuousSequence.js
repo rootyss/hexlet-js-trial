7e66c0ec7d4297796929c6a237cf402198c7987a
rooty <jaxent@yandex.ru>
2020-11-05 21:09:07 +0300

    add isContinuousSequence.js

--- isContinuousSequence.js
+++ isContinuousSequence.js
@@ -1,3 +1,7 @@
 const isContinuousSequence = (sequence) => {
-	
-}
+  if (sequence.length < 2) return false;
+  for (let i = 0; i < sequence.length - 1; i += 1) {
+    if (Math.abs(sequence[i + 1] - sequence[i] !== 1)) return false;
+  }
+  return true;
+};
