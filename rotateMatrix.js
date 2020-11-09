c9765ccc301dcd7a5a46bfe3e3034e5a2eb0c7c6
rooty <jaxent@yandex.ru>
2020-11-09 21:56:45 +0300

    add rotateMatrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -1,16 +1,23 @@
-const rotateLeft = (matrix) => {
-	const resultMatrix = [];
-	for (let i = 0; i < matrix.length; i += 1) {
-		for (let j = 0; j < matrix[i].length; j += 1) {
-			
-			
-		}
-	}
-};
+const rotate = (matrix, direction) => {
+  const rowsCount = matrix.length;
+  const [firstRow] = matrix;
+  const columnsCount = firstRow.length;
+  const rotated = [];
 
+  for (let row = 0; row < columnsCount; row += 1) {
+    rotated[row] = [];
+    for (let column = 0; column < rowsCount; column += 1) {
+      rotated[row][column] = direction === 'left'
+        ? matrix[column][columnsCount - row - 1]
+        : matrix[rowsCount - column - 1][row];
+    }
+  }
+  return rotated;
+};
 
 const matrix = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
   [9, 0, 1, 2],
 ];
+
