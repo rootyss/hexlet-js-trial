22428a9be43f330f68ea3eb8c117df157450ffc2
rootyss <jaxent@yandex.ru>
2020-11-16 17:07:35 +0300

    'add_getLongestLength.js'

--- getLongestLength.js
+++ getLongestLength.js
@@ -1,3 +1,31 @@
 const getLongestLength = (str) => {
-  
+  let uniqSymbol = [];
+  let maxLength = 0;
+  for (let i = 0; i < str.length; i += 1) {
+  	for (let k = i; k < str.length; k += 1){
+  	if (uniqSymbol.indexOf(str[k]) === -1) {
+      uniqSymbol.push(str[k]);
+  	  }
+    if (uniqSymbol.indexOf(str[k]) >= 0) {
+    	if(uniqSymbol.length > maxLength) {
+    		maxLength = uniqSymbol.length;
+    		uniqSymbol = [];
+    		uniqSymbol.push(str[k]);
+    	}
+    }
+    
+  	}
+  	}
+  return maxLength;
 };
+
+
+
+
+ console.log(getLongestLength('jabjcdel'), 7) ;
+ console.log(getLongestLength('abcddef'), 4);
+ console.log(getLongestLength('abbccddeffg'), 3);
+ console.log(getLongestLength('abcd'), 4);
+ console.log(getLongestLength('1234561qweqwer'), 9);
+ console.log(getLongestLength('1234561qweqwerqer'), 9);
+ console.log(getLongestLength(''), 0);
